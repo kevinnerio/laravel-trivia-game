14e03e9c84328cda1a93c72645219146cfde6fa2
rooftop-kev <100609668+rooftop-kev@users.noreply.github.com>
2025-05-04 14:34:09 -0700

    fix: adjusted some visuals and styling on the site.

--- trivia-laravel/resources/views/layouts/navigation.blade.php
+++ trivia-laravel/resources/views/layouts/navigation.blade.php
@@ -5,8 +5,10 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="/">
+                        <svg class="w-full max-w-[300px] text-[#0066ff] dark:text-[#F61500] transition-all translate-y-0 opacity-100 max-w-none duration-750 starting:opacity-0 starting:translate-y-6" viewBox="0 0 600 120" xmlns="http://www.w3.org/2000/svg">
+                            <text x="0" y="90" font-family="Arial Black, sans-serif" font-size="32" fill="currentColor">PUB TRIVIA</text>
+                        </svg>
                     </a>
                 </div>
 
@@ -22,15 +24,18 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
+                        @auth
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </button>
+                        @else
+                            <!-- nothing or optional message -->
+                        @endauth
                     </x-slot>
 
                     <x-slot name="content">
@@ -75,26 +80,43 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <div class="font-medium text-base text-gray-800 dark:text-gray-200">
+                    @auth
+                        <div>{{ Auth::user()->name }}</div>
+                    @else
+                        <!-- nothing or optional message -->
+                    @endauth
+                </div>
+                <div class="font-medium text-sm text-gray-500">
+                    @auth
+                        <div>{{ Auth::user()->email }}</div>
+                    @else
+                        <!-- nothing or optional message -->
+                    @endauth
+                </div>
             </div>
 
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
+            @auth
+                <div class="mt-3 space-y-1">
+                    <x-responsive-nav-link :href="route('profile.edit')">
+                        {{ __('Profile') }}
                     </x-responsive-nav-link>
-                </form>
-            </div>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
+            @else
+            <!-- nothing or optional message -->
+            @endauth 
+
         </div>
     </div>
 </nav>
