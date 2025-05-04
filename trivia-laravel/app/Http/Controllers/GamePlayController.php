70c0a802acc70e6a69b723d72fc330ae30d6f3a2
rooftop-kev <100609668+rooftop-kev@users.noreply.github.com>
2025-05-03 21:30:49 -0700

    feature: adding api for question fetching.

--- trivia-laravel/app/Http/Controllers/GamePlayController.php
+++ trivia-laravel/app/Http/Controllers/GamePlayController.php
@@ -3,28 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Game;
+use App\Services\TriviaApiService;
+use Illuminate\Support\Facades\Session;
 
-class GamePlayController extends Controller
+class GameController extends Controller
 {
+    protected $triviaApi;
 
-    # Needed in order to add user(s) to game. 
-    public function joinGame(Request $request, $gameId)
+    public function __construct(TriviaApiService $triviaApi)
     {
-        $game = Game::findOrFail($gameId);
-        $player = Player::firstOrCreate(['email' => $request->email]);
-
-        $game->players()->attach($player->id);
-
-        return response()->json(['message' => 'Joined game!']);
+        $this->triviaApi = $triviaApi;
     }
 
-    # Needed in order to start a game. 
-    public function startGame($gameId)
+    public function start()
     {
-        $game = Game::findOrFail($gameId);
-        $game->status = 'started';
+        // Fetch trivia questions from the API
+        $questions = $this->triviaApi->fetchQuestions();
+
+        // Store questions in session or temp storage (you can also save them to the database if needed)
+        Session::put('questions', $questions['results']);
+        
+        // Create a new game entry
+        $game = new Game();
+        $game->session_id = Session::getId();
+        $game->current_question_index = 0;
+        $game->score = 0;
         $game->save();
 
-        return response()->json(['message' => 'Game started!']);
+        return redirect()->route('game.question', $game->id);
+    }
+
+    public function showQuestion(Game $game)
+    {
+        $questions = Session::get('questions');
+        $question = $questions[$game->current_question_index];
+
+        return view('game.question', compact('game', 'question'));
+    }
+
+    public function submitAnswer(Request $request, Game $game)
+    {
+        $questions = Session::get('questions');
+        $currentQuestion = $questions[$game->current_question_index];
+
+        $isCorrect = $request->input('answer') === $currentQuestion['correct_answer'];
+        if ($isCorrect) {
+            $game->score += 1;
+        }
+
+        $game->current_question_index += 1;
+        $game->save();
+
+        if ($game->current_question_index >= count($questions)) {
+            return redirect()->route('game.result', $game->id);
+        }
+
+        return redirect()->route('game.question', $game->id);
+    }
+
+    public function result(Game $game)
+    {
+        return view('game.result', compact('game'));
     }
 }
--- trivia-laravel/routes/web.php
+++ trivia-laravel/routes/web.php
@@ -10,3 +10,9 @@ Route::get('/', function () {
 });
 
 Route::post('/games/{gameId}/join', [GameController::class, 'joinGame']);
+
+# Testing routes 
+Route::get('/game/start', [GameController::class, 'start'])->name('game.start');
+Route::get('/game/{game}/question', [GameController::class, 'showQuestion'])->name('game.question');
+Route::post('/game/{game}/answer', [GameController::class, 'submitAnswer'])->name('game.answer');
+Route::get('/game/{game}/result', [GameController::class, 'result'])->name('game.result');
